6f05b3dac828f042f709a512d80480f5d33514e1
Juampicosta123 <juampicosta81@gmail.com>
2025-04-28 21:46:23 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>StockMaster</h1>
+      <p>Welcome to StockMaster, your go-to app for stock management.</p>
+      <button>Get Started</button>
+    </>
+  )
+}
+
+export default App
