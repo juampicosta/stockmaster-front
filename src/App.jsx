126497ac3e4dcdbe7b3f62e2ebae4e768a30fdfc
Juampicosta123 <juampicosta81@gmail.com>
2025-04-30 18:11:02 -0300

    Add Layout component and wrap routes in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,19 @@ import Articulos from './pages/Articulos'
 import Proveedores from './pages/Proveedores'
 import OrdenesDeCompra from './pages/OrdenesDeCompra'
 import Ventas from './pages/Ventas'
+import Layout from './layouts/Layout'
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Articulos />} />
-      <Route path='/articulos' element={<Articulos />} />
-      <Route path='/proveedores' element={<Proveedores />} />
-      <Route path='/ordenes-de-compra' element={<OrdenesDeCompra />} />
-      <Route path='/ventas' element={<Ventas />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path='/' element={<Articulos />} />
+        <Route path='/articulos' element={<Articulos />} />
+        <Route path='/proveedores' element={<Proveedores />} />
+        <Route path='/ordenes-de-compra' element={<OrdenesDeCompra />} />
+        <Route path='/ventas' element={<Ventas />} />
+      </Routes>
+    </Layout>
   )
 }
 
---
+++ src/layouts/Layout.jsx
@@ -0,0 +1,11 @@
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <header>Header</header>
+      <main>{children}</main>
+      <footer>Footer</footer>
+    </div>
+  )
+}
+
+export default Layout
