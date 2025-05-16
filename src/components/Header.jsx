9001114be5e2d0093541707981409b5c4417cca4
Juampicosta123 <juampicosta81@gmail.com>
2025-05-16 17:34:17 -0300

    Refactor Header component: enhance layout and styling, migrate from layouts to components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,55 +1,65 @@
 const Header = () => {
   return (
     <>
-      <header className='bg-beige text-orange-900 sticky top-0 z-50 py-2 flex items-center justify-between px-6 shadow-xl'>
-        <div className='flex justify-center items-center'>
+      <header className='bg-beige text-orange-900 rounded-b-lg rounded-bl-none sticky top-0 z-50 h-24 flex items-center justify-between px-6 shadow-xl'>
+        <div className='flex items-center space-x-4'>
           <img
             src='/LogoSM.jpg'
             alt='Logo'
-            className='size-12 rounded-full shadow-2xl'
+            className='w-20 h-20 rounded-full shadow-md'
           />
+
+          <span className='text-md font-bold'>Gestor de Inventarios</span>
         </div>
 
-        <nav className='flex items-center space-x-4 text-md'>
-          <a href='/' className='font-semibold hover:text-orange-600'>
+        <nav className='flex items-center space-x-6'>
+          <a href='/' className='text-lg font-semibold hover:text-orange-600'>
             Inicio
           </a>
-          <a href='/Articulos' className='font-semibold hover:text-orange-600'>
+          <a
+            href='/Articulos'
+            className='text-lg font-semibold hover:text-orange-600'
+          >
             Artículos
           </a>
           <a
             href='/OrdenesDeCompra'
-            className='font-semibold hover:text-orange-600'
+            className='text-lg font-semibold hover:text-orange-600'
           >
             Órdenes de Compra
           </a>
           <a
             href='/Proveedores'
-            className='font-semibold hover:text-orange-600'
+            className='text-lg font-semibold hover:text-orange-600'
           >
             Proveedores
           </a>
-          <a href='/Ventas' className='font-semibold hover:text-orange-600'>
+          <a
+            href='/Ventas'
+            className='text-lg font-semibold hover:text-orange-600'
+          >
             Ventas
           </a>
         </nav>
-        <div className='animate-heartBeat relative inline-block'>
-          <span className='absolute top-1 right-1 flex size-2'>
-            <span className='absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75'></span>
-            <span className='relative inline-flex size-2 rounded-full bg-red-500'></span>
-          </span>
-          <div className='rounded-full hover:bg-arena transition-colors duration-200 p-2'>
-            <svg
-              class='w-6 h-6 text-amber-950'
-              aria-hidden='true'
-              xmlns='http://www.w3.org/2000/svg'
-              width='24'
-              height='24'
-              fill='currentColor'
-              viewBox='0 0 24 24'
-            >
-              <path d='M17.133 12.632v-1.8a5.406 5.406 0 0 0-4.154-5.262.955.955 0 0 0 .021-.106V3.1a1 1 0 0 0-2 0v2.364a.955.955 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C6.867 15.018 5 15.614 5 16.807 5 17.4 5 18 5.538 18h12.924C19 18 19 17.4 19 16.807c0-1.193-1.867-1.789-1.867-4.175ZM8.823 19a3.453 3.453 0 0 0 6.354 0H8.823Z' />
-            </svg>
+
+        <div className='flex flex-row items-center space-x-0'>
+          <img
+            src='/Deco2.png'
+            alt='Deco'
+            className='w-32 h-8 object-contain'
+          />
+          <div className='animate-heartBeat relative inline-block'>
+            <span className='absolute top-1 right-1 flex size-3'>
+              <span className='absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75'></span>
+              <span className='relative inline-flex size-3 rounded-full bg-red-500'></span>
+            </span>
+            <div className='rounded-full hover:bg-arena transition-colors duration-200 p-2'>
+              <img
+                src='/Alerta4.png'
+                alt='Alertas'
+                className='w-10 h-10 rounded-full animate-wiggle'
+              />
+            </div>
           </div>
         </div>
       </header>
