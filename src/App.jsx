61936dc7a8fe506c8d3982e5c5775df86830db82
Juampicosta123 <juampicosta81@gmail.com>
2025-05-22 12:21:37 -0300

    Refactor article management: replace Articulos component with ListaArticulos, add AltaArticulos, DetalleArticulo, and EditarArticulo components; update API service for article registration and deletion

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import { Route, Routes } from 'react-router'
 import './App.css'
-import Articulos from './pages/Articulos'
+import ListaArticulos from './pages/Articulos/ListaArticulos'
 import Proveedores from './pages/Proveedores'
 import OrdenesDeCompra from './pages/OrdenesDeCompra'
 import Ventas from './pages/Ventas'
 import Layout from './layouts/Layout'
 import Inicio from './pages/Inicio'
+import AltaArticulos from './pages/Articulos/AltaArticulos'
+import DetalleArticulo from './pages/Articulos/DetalleArticulo'
+import EditarArticulo from './pages/Articulos/EditarArticulo'
 
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route path='/' element={<Articulos />} />
-        <Route path='/articulos' element={<Articulos />} />
+        <Route path='/' element={<ListaArticulos />} />
+        <Route path='/articulos' element={<ListaArticulos />} />
+        <Route path='/articulos/alta' element={<AltaArticulos />} />
+        <Route path='/articulos/editar/:id' element={<EditarArticulo />} />
+        <Route path='/articulos/detalle/:id' element={<DetalleArticulo />} />
         <Route path='/proveedores' element={<Proveedores />} />
         <Route path='/ordenes-de-compra' element={<OrdenesDeCompra />} />
         <Route path='/ventas' element={<Ventas />} />
