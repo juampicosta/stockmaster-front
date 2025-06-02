d4e452228696e0cff05bc52909bbde18eebca473
Juampicosta123 <juampicosta81@gmail.com>
2025-06-02 13:09:41 -0300

    Refactor AltaOrden and ListaOrdenes components: improve error handling in API calls, update state management for orders, and enhance logging for debugging

--- src/pages/OrdenesDeCompra/ListaOrdenes.jsx
+++ src/pages/OrdenesDeCompra/ListaOrdenes.jsx
@@ -3,7 +3,11 @@ import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { MdAddCircle, MdEdit } from 'react-icons/md'
 import { IoMdPaper } from 'react-icons/io'
-import { cancelarOrden, obtenerOrdenes } from '../../services/apiOrdenes'
+import {
+  advanceOrderState,
+  cancelarOrden,
+  obtenerOrdenes
+} from '../../services/apiOrdenes'
 import { LuBoxes } from 'react-icons/lu'
 import { BsCash, BsClock } from 'react-icons/bs'
 import { BiUser } from 'react-icons/bi'
@@ -21,7 +25,7 @@ const ListaOrdenes = () => {
         return toast.error(errorMsg)
       }
 
-      setOrdenes(data.content)
+      setOrdenes(data || [])
     }
 
     fetchOrdenes()
@@ -54,16 +58,17 @@ const ListaOrdenes = () => {
   }
 
   const handleChangeState = async (id, newStateId) => {
-    /* const { data, errorMsg } = await cambiarEstadoOrden(id, newStateId)
-    if (errorMsg) {
-      return toast.error(errorMsg)
-    } */
     if (newStateId === 4) {
       const confirm = window.confirm(
         '¿Estás seguro de que deseas cancelar esta orden?'
       )
       if (!confirm) return
       const { errorMsg } = await cancelarOrden(id)
+      if (errorMsg) {
+        return toast.error(errorMsg)
+      }
+    } else {
+      const { data, errorMsg } = await advanceOrderState(id)
       if (errorMsg) {
         return toast.error(errorMsg)
       }
@@ -91,6 +96,8 @@ const ListaOrdenes = () => {
     toast.success('Estado de la orden actualizado correctamente')
   }
 
+  console.log('Ordenes:', ordenes)
+
   return (
     <div className='bg-white min-h-screen p-8'>
       <h1 className='text-4xl font-extrabold text-orange-900 mb-8 tracking-tight'>
@@ -109,13 +116,13 @@ const ListaOrdenes = () => {
           <h2 className='text-xl font-semibold text-orange-800 mb-4'>
             Lista de Ordenes de Compra
           </h2>
-          {ordenes.length === 0 ? (
+          {ordenes?.length === 0 ? (
             <p className='text-gray-500'>
               Aún no hay ordenes de compra creadas.
             </p>
           ) : (
             <ul className='space-y-4'>
-              {ordenes.map((ord) => (
+              {ordenes?.map((ord) => (
                 <li
                   key={ord.id}
                   className='flex justify-between items-center bg-white shadow-lg border border-yellow-200 rounded-xl p-6'
