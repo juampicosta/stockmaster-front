a620587f8a4beeea4d8a3d6ace0681b58f9c7be3
Juampicosta123 <juampicosta81@gmail.com>
2025-06-22 11:43:44 -0300

    Filter out inactive providers in the EditarArticulo component's provider selection

--- src/pages/Articulos/EditarArticulo.jsx
+++ src/pages/Articulos/EditarArticulo.jsx
@@ -168,14 +168,16 @@ const EditarArticulo = () => {
             className='w-full px-3 py-2 bg-beige text-black border border-orange-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-orange-500 transition-colors duration-200'
           >
             <option value=''>Seleccionar proveedor</option>
-            {articulo.articuloProveedores.map((articuloProveedor) => (
-              <option
-                key={articuloProveedor.proveedor.id}
-                value={articuloProveedor.proveedor.id}
-              >
-                {articuloProveedor.proveedor.razonSocial}
-              </option>
-            ))}
+            {articulo.articuloProveedores
+              .filter((ap) => !ap.proveedor.fechaHoraBaja)
+              .map((articuloProveedor) => (
+                <option
+                  key={articuloProveedor.proveedor.id}
+                  value={articuloProveedor.proveedor.id}
+                >
+                  {articuloProveedor.proveedor.razonSocial}
+                </option>
+              ))}
           </select>
         </label>
 
