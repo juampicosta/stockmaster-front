92c0c5bce5c3de77c2a4127cbd4063590b2693df
Juampicosta123 <juampicosta81@gmail.com>
2025-06-22 22:10:19 -0300

    Refactor DetalleOrden component: simplify article and provider display, remove redundant sections, and enhance layout

--- src/pages/OrdenesDeCompra/DetalleOrden.jsx
+++ src/pages/OrdenesDeCompra/DetalleOrden.jsx
@@ -1,11 +1,12 @@
 import { useParams } from 'react-router-dom'
-import { MdEmail, MdPhone, MdPerson } from 'react-icons/md'
+import { MdPerson } from 'react-icons/md'
 import { LuBoxes } from 'react-icons/lu'
 import { useEffect, useState } from 'react'
 import { obtenerOrden } from '../../services/apiOrdenes'
 import { toast } from 'sonner'
 import { getOrderStateColor } from '../../utils/getOrderStateColor'
 import { BsCash, BsClock } from 'react-icons/bs'
+import { BiBox } from 'react-icons/bi'
 
 const DetalleOrden = () => {
   const { id } = useParams()
@@ -55,6 +56,22 @@ const DetalleOrden = () => {
           <span className='text-md text-orange-800'>${orden?.montoTotal}</span>
         </div>
 
+        <div className='flex items-center justify-start gap-2 text-orange-800'>
+          <BiBox className='text-xl text-orange-700' />
+          <strong>Articulo: </strong>
+          <span className='text-md text-orange-800'>
+            {orden?.articulo?.descripcion}
+          </span>
+        </div>
+
+        <div className='flex items-center justify-start gap-2 text-orange-800'>
+          <MdPerson className='text-xl text-orange-700' />
+          <strong>Proveedor: </strong>
+          <span className='text-md text-orange-800'>
+            {orden?.proveedor?.razonSocial}
+          </span>
+        </div>
+
         <div
           className={`flex items-center justify-start gap-2 text-orange-800`}
         >
@@ -67,66 +84,6 @@ const DetalleOrden = () => {
           >
             {orden?.estadoOrdenCompra}
           </span>
-        </div>
-      </div>
-
-      <h2 className='text-2xl font-bold text-orange-800 mb-4'>Articulo</h2>
-      <div className='bg-white shadow-lg border border-yellow-200 rounded-xl p-6 mb-8'>
-        <div className='flex items-center mb-4'>
-          <div className='flex items-center justify-center w-12 h-12 bg-orange-100 rounded-full mr-4'>
-            <LuBoxes className='text-3xl text-orange-700' />
-          </div>
-          <span className='text-2xl font-semibold text-orange-800'>
-            {orden?.articulo?.descripcion}
-          </span>
-        </div>
-        <div className='flex items-center mb-2 gap-2 text-orange-800'>
-          <strong>Precio Unitario: </strong>
-          <span className='text-md text-gray-700'>
-            ${orden?.articulo?.precioUnitario}
-          </span>
-        </div>
-        <div className='flex items-center mb-2 gap-2 text-orange-800'>
-          <strong>Costo de Pedido: </strong>
-          <span className='text-md text-gray-700'>
-            ${orden?.articulo?.costoPedido}
-          </span>
-        </div>
-        <div className='flex items-center gap-2 text-orange-800'>
-          <strong>Demora de Entrega (Días) : </strong>
-          <span className='text-md text-gray-700'>
-            {orden?.articulo?.demoraEntrega} días
-          </span>
-        </div>
-      </div>
-
-      <h2 className='text-2xl font-bold text-orange-800 mb-4'>Proveedor</h2>
-      <div className='bg-white shadow-lg border border-yellow-200 rounded-xl p-6 mb-8'>
-        <div className='flex items-center mb-4'>
-          <div className='flex items-center justify-center w-12 h-12 bg-orange-100 rounded-full mr-4'>
-            <MdPerson className='text-3xl text-orange-700' />
-          </div>
-          <span className='text-2xl font-semibold text-orange-800'>
-            {orden?.proveedor?.razonSocial}
-          </span>
-        </div>
-        <div className='flex items-center mb-2'>
-          <MdEmail className='text-xl text-orange-500 ml-3 mr-2' />
-          <a
-            href={`mailto:${orden?.proveedor?.email}`}
-            className='text-md text-gray-700'
-          >
-            {orden?.proveedor?.email}
-          </a>
-        </div>
-        <div className='flex items-center'>
-          <MdPhone className='text-xl text-orange-500 ml-3 mr-2' />
-          <a
-            href={`tel:${orden?.proveedor?.telefono}`}
-            className='text-md text-gray-700'
-          >
-            {orden?.proveedor?.telefono}
-          </a>
         </div>
       </div>
     </div>
